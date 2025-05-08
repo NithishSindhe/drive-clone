2361d88a14a04eb89bdbccef109b115df3ff36f5
Nithish Sindhe - nsindhe <nithishsindhe@gmail.com>
2025-05-09 00:16:40 +0530

    singlestore suff/incomplete

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,49 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from 'drizzle-orm/mysql2';
+import { createPool, type Pool } from "mysql2/promise";
 
-//import { env } from "~/env";
+import { env } from "@/env";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
-};
+const globalForDb = globalThis as unknown as { conn: Pool | undefined; };
 
-export const client =
-  globalForDb.client ?? createClient({ url: process.env.DATABASE_URL! });
-if (process.env.NODE_ENV !== "production") globalForDb.client = client;
+export const conn: Pool =
+  globalForDb.conn ??
+  createPool({
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
+    maxIdle: 0,
+  });
+if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
-export const db = drizzle(client, { schema });
+conn.addListener("error", (err) => {
+  console.error("Database connection error:", err);
+});
+
+//test connection to database
+const pool = createPool({
+  host: env.SINGLESTORE_HOST,
+  port: parseInt(env.SINGLESTORE_PORT),
+  user: env.SINGLESTORE_USER,
+  password: env.SINGLESTORE_PASS,
+  database: env.SINGLESTORE_DB_NAME,
+  ssl: {}, // optional for SingleStore Cloud
+  maxIdle: 0,
+});
+
+async function testDbConnection() {
+  try {
+    const conn = await pool.getConnection();
+    console.log("✅ Successfully connected to SingleStore.");
+    conn.release();
+  } catch (error) {
+    console.error("❌ Failed to connect to SingleStore:", error);
+  }
+}
+
+testDbConnection();
+
+export const db = drizzle(conn, { schema,mode: "default" });
